951b09ef767301dd061b6baa44ee22da4346a8c0
Ruslan Kutliakhmetov <rkutliakhmetov@phoenixit.ru>
2023-08-29 15:20:03 +0500

    feat: add query client

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,19 @@
 import { Routing } from '@/pages';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Routing />
-    </>
+    </QueryClientProvider>
   );
 }
 
