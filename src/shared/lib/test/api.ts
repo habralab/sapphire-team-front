7774d1a321ab0af0cace2d2e2db98f7701cbfad5
Ruslan Kutliakhmetov <kosid2@gmail.com>
2023-09-03 12:46:42 +0500

    refactor: исправление eslint при сохранения файла + настройки тестов

---
+++ src/shared/lib/test/api.ts
@@ -0,0 +1,7 @@
+import { server } from '@/shared/api/mock';
+
+beforeAll(() => server.listen({ onUnhandledRequest: 'error' }));
+
+afterEach(() => server.resetHandlers());
+
+afterAll(() => server.close());
---
+++ src/shared/lib/test/zustand.ts
@@ -0,0 +1,25 @@
+import { StateCreator } from 'zustand';
+import { act } from '@testing-library/react';
+
+type ZustandModel = typeof import('zustand');
+
+const storeResetFns = vi.hoisted(() => new Set<() => void>());
+
+vi.mock('zustand', async (zustandOriginal: () => Promise<ZustandModel>) => {
+  const zustand = await zustandOriginal();
+
+  const createStore =
+    () =>
+    <S>(createState: StateCreator<S>) => {
+      const store = zustand.createStore(createState);
+      const initialState = store.getState();
+      storeResetFns.add(() => store.setState(initialState, true));
+      return store;
+    };
+
+  return { ...zustand, createStore };
+});
+
+beforeEach(() => {
+  act(() => storeResetFns.forEach((resetFn) => resetFn()));
+});
