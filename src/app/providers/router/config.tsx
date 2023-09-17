12db988057eb91bf9834fe03753f73bc387cc6d0
Ruslan Kutliakhmetov <kosid2@gmail.com>
2023-09-17 23:47:25 +0500

    fix: redirect to 404

--- src/app/providers/router/config.tsx
+++ src/app/providers/router/config.tsx
@@ -1,8 +1,13 @@
+import { Navigate } from 'react-router-dom';
+
 import { BlankPage } from '~/pages/BlankPage';
 import { NotFound } from '~/pages/NotFound';
 
 const appClosed = [{ path: '*', element: <BlankPage /> }];
 
-const normalRoutes = [{ path: '*', element: <NotFound /> }];
+const normalRoutes = [
+  { path: '404', element: <NotFound /> },
+  { path: '*', element: <Navigate to={'404'} replace /> },
+];
 
 export const routes = import.meta.env.VITE_CLOSED ? appClosed : normalRoutes;
