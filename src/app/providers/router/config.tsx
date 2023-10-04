16cf7b6cd9fd6f1bf41ba947618b8406094d84ed
diewerkself <redead29@gmail.com>
2023-10-04 21:30:24 +0300

    refactor: создание алиаса на страницу

--- src/app/providers/router/config.tsx
+++ src/app/providers/router/config.tsx
@@ -7,9 +7,9 @@ import { SearchPage } from '~/pages/SearchPage';
 const appClosed = [{ path: '*', element: <BlankPage /> }];
 
 const normalRoutes = [
+  { path: '/search', element: <SearchPage /> },
   { path: '404', element: <NotFound /> },
-  { path: '*', element: <SearchPage /> },
-  // { path: '*', element: <Navigate to={'404'} replace /> },
+  { path: '*', element: <Navigate to={'404'} replace /> },
 ];
 
 export const routes = import.meta.env.VITE_CLOSED ? appClosed : normalRoutes;
