6bc8f11fc41cb2643f87f088cd98363cac0236fa
diewerkself <redead29@gmail.com>
2023-10-07 20:27:25 +0300

    feat: добавление layout page

--- src/app/providers/router/config.tsx
+++ src/app/providers/router/config.tsx
@@ -1,12 +1,14 @@
 import { Navigate } from 'react-router-dom';
 
 import { BlankPage } from '~/pages/BlankPage';
+import { Layout } from '~/pages/Layout';
 import { NotFound } from '~/pages/NotFound';
 import { ProfilePage } from '~/pages/ProfilePage';
 
 const appClosed = [{ path: '*', element: <BlankPage /> }];
 
 const normalRoutes = [
+  { path: '/test', element: <Layout Mobile={<NotFound />} Desktop={<BlankPage />} /> },
   { path: '404', element: <NotFound /> },
   { path: '*', element: <Navigate to={'404'} replace /> },
   { path: '/profile', element: <ProfilePage /> },
