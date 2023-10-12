758eead9b46947d7c68e9220c70412e021c8cbc2
diewerkself <redead29@gmail.com>
2023-10-12 22:24:44 +0300

    refactor: меню для десктопа

--- src/shared/ui/Divider/Divider.tsx
+++
@@ -1,5 +0,0 @@
-import { Divider as ChakraDivider, DividerProps } from '@chakra-ui/react';
-
-export const Divider = (props: DividerProps) => {
-  return <ChakraDivider borderColor="gray.300" {...props} />;
-};
