872971fd650f1681a7f9777a921eef157cf70e93
diewerkself <redead29@gmail.com>
2023-10-13 19:49:52 +0300

    refactor: изменение отступов для карточки отзыва

--- src/entities/user/reviews/Reviews.tsx
+++ src/entities/user/reviews/Reviews.tsx
@@ -12,13 +12,19 @@ export function Reviews(props: ReviewType) {
 
   return (
     <VStack align="stretch" spacing={0} p={5} mb={4} borderRadius="2xl" bg="white">
-      <Flex align="center" justifyContent="space-between">
-        <SText variant="caption">3 сентября 2022</SText>
+      <Flex align="center" justifyContent="space-between" mb={3}>
+        <SText variant="caption" mb={0}>
+          3 сентября 2022
+        </SText>
         {rating}
       </Flex>
-      <Flex direction="column" py={3} gap={1}>
-        <SText variant="h2">Михаил Шафутинский</SText>
-        <SText variant="caption">Участник проекта</SText>
+      <Flex direction="column" pb={3} gap={1}>
+        <SText variant="h2" mb={0}>
+          Михаил Шафутинский
+        </SText>
+        <SText variant="caption" mb={0}>
+          Участник проекта
+        </SText>
         <SLink to="#">Сервис онлайн-образования</SLink>
       </Flex>
       <SText>
