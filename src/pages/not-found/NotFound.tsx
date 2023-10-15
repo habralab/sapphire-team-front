a75cb0fff0f336b09b12395cc7c58b9086959f62
diewerkself <redead29@gmail.com>
2023-10-15 19:55:59 +0300

    refactor: страница не найдено

--- src/pages/not-found/NotFound.tsx
+++ src/pages/not-found/NotFound.tsx
@@ -1,16 +1,54 @@
-import { Heading, Text } from '@chakra-ui/react';
+import { Flex, Heading, Icon, IconButton, Text } from '@chakra-ui/react';
+import { BsArrowRightShort } from 'react-icons/bs';
+import { useNavigate } from 'react-router-dom';
 
-import { SLink } from '~/shared/ui/SLink';
+import { PATHS } from '~/shared/lib/router';
+
+import NotFound from './notFound.svg';
 
 export const NotFoundPage = () => {
+  const navigate = useNavigate();
   return (
-    <>
-      <Heading variant="h1" as="h1" mb={4}>
+    <Flex height="100vh" direction="column" justifyContent="center" alignItems="center">
+      <Heading
+        variant="h1"
+        as="h1"
+        fontSize={['8xl', '8xl', '9xl']}
+        mb={4}
+        textAlign="center"
+        position="relative"
+        _after={{
+          base: {
+            content: `url(${NotFound})`,
+            position: 'absolute',
+            top: '-14',
+            left: '-6',
+          },
+          md: {
+            top: '-20 !important',
+            left: '-10 !important',
+          },
+        }}
+      >
         404
       </Heading>
-      <Text>
-        Страница не найдена, <SLink to="/">перейти на главную</SLink>
-      </Text>
-    </>
+      <Heading as="h2" fontSize={['2xl', '2xl', '4xl']} textAlign="center" pb={10}>
+        Упс!.. Похоже, такой страницы не существует
+      </Heading>
+      <IconButton
+        aria-label="main-page"
+        onClick={() => {
+          navigate(PATHS.root);
+        }}
+        padding={7}
+        maxW={'sm'}
+        icon={
+          <Flex fontSize={['sm', 'sm', 'md']} fontWeight="normal" gap={2}>
+            <Text lineHeight="160%">Вернуться на главную страницу</Text>
+            <Icon as={BsArrowRightShort} />
+          </Flex>
+        }
+      />
+    </Flex>
   );
 };
