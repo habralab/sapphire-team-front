c701b05811eaa3b33fcbec95c7272c405db3a9be
Ruslan Kutliakhmetov <kosid2@gmail.com>
2023-10-18 00:30:09 +0500

    fix: layout уведомления для мобильного устройства

--- src/pages/notification/NotificationPage.tsx
+++ src/pages/notification/NotificationPage.tsx
@@ -1,30 +1,49 @@
-import { IconButton, Icon, Stack, Flex, Heading } from '@chakra-ui/react';
+import {
+  IconButton,
+  Icon,
+  Stack,
+  Flex,
+  Heading,
+  Portal,
+  Container,
+  Divider,
+} from '@chakra-ui/react';
 import { FiChevronLeft } from 'react-icons/fi';
 import { useNavigate } from 'react-router-dom';
 
 import { NotificationList } from '~/widgets/notification-list';
 
+import { useLayoutRefs } from '~/shared/hooks';
+
 export function NotificationPage() {
   const navigate = useNavigate();
+  const layout = useLayoutRefs();
   const goBack = () => {
     navigate(-1);
   };
 
   return (
     <Stack spacing={0}>
-      <Flex alignItems="center" mb={8} gap={4}>
-        <IconButton
-          aria-label="back"
-          icon={<Icon as={FiChevronLeft} />}
-          variant="flat"
-          onClick={goBack}
-          minW={6}
-          h={6}
-        />
-        <Heading variant="h2" as="h1" mb={0}>
-          Уведомления
-        </Heading>
-      </Flex>
+      {layout?.header && (
+        <Portal containerRef={layout.header}>
+          <Container pos="relative">
+            <Flex alignItems="center" my={4} gap={4}>
+              <IconButton
+                aria-label="back"
+                icon={<Icon as={FiChevronLeft} />}
+                variant="flat"
+                onClick={goBack}
+                minW={6}
+                h={6}
+              />
+              <Heading variant="h2" as="h1" mb={0}>
+                Уведомления
+              </Heading>
+            </Flex>
+            <Divider variant="light" position="absolute" left={0} right={0} />
+          </Container>
+        </Portal>
+      )}
       <NotificationList />
     </Stack>
   );
