1151e59b8fa94ed497e32ede3425f1742194fc83
Ruslan Kutliakhmetov <kosid2@gmail.com>
2023-10-23 17:28:26 +0500

    fix: concurently запуск задач

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,10 +1,11 @@
 module.exports = {
   '*.{ts,tsx}': (filenames) => {
     return [
-      `yarn tslint`,
-      `vitest related --run ${filenames.join(' ')}`,
-      `yarn eslint --fix ${filenames.join(' ')}`,
-      `yarn prettier --write ${filenames.join(' ')}`,
+      `concurrently "yarn tslint" "vitest related --run ${filenames.join(
+        ' ',
+      )}" "yarn eslint --fix ${filenames.join(
+        ' ',
+      )}" "yarn prettier --write ${filenames.join(' ')}"`,
     ];
   },
 };
