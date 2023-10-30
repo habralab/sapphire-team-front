2973fd2d5df84edc870158d64215f191281478d4
diewerkself <redead29@gmail.com>
2023-10-30 19:22:58 +0300

    feat: страница создания проекта

--- src/pages/projects/ui/AddProjectPage.tsx
+++ src/pages/projects/ui/AddProjectPage.tsx
@@ -1,19 +1,33 @@
-import { Flex, SimpleGrid, Heading, Container } from '@chakra-ui/react';
+import { ChevronLeftIcon } from '@chakra-ui/icons';
+import { Flex, Button, Heading, Container, IconButton, Icon } from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
 
 export const AddProjectPage = () => {
+  const navigate = useNavigate();
   return (
     <Container maxW="md" mb={4}>
       <Flex alignItems="center" justifyContent="space-between" my={4}>
-        <Flex alignItems="baseline">
-          <CloseButton onClick={onClose} mr={2} />
+        <Flex alignItems="center">
+          <IconButton
+            variant="ghost"
+            aria-label="projects"
+            onClick={() => {
+              navigate(-1);
+            }}
+            gap={2}
+            flexShrink="0"
+            padding={['0', '0', '4']}
+            icon={<Icon as={ChevronLeftIcon} fontSize="2xl" />}
+          />
           <Heading variant="h2" mb={0}>
-            Фильтры
+            Создать проект
           </Heading>
         </Flex>
-        <Button variant="flat" fontSize="sm" fontWeight="500" colorScheme="purple">
-          Сбросить
+        <Button variant="flat" fontSize="xs" fontWeight="500" colorScheme="purple">
+          Сохранить и выйти
         </Button>
       </Flex>
+
       <Container maxW="md" py={6} bg="bg" position="sticky" bottom="0" mt="auto">
         <Button fontSize="sm" fontWeight="600" w="full">
           Продолжить
