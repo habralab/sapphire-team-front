594337755376f81d20bfe7ab1734365535bf2d7d
Ruslan Kutliakhmetov <kosid2@gmail.com>
2023-11-01 11:29:57 +0500

    fix: кликабельный лого

--- src/shared/ui/Logo/LogoDesktop.tsx
+++ src/shared/ui/Logo/LogoDesktop.tsx
@@ -1,13 +1,22 @@
-import { Flex, Icon } from '@chakra-ui/react';
+import { Icon, Link } from '@chakra-ui/react';
+import { Link as ReactLink } from 'react-router-dom';
 
 import { ReactComponent as IconLogo } from './logoDesktop.svg';
 import { ReactComponent as NameLogo } from './nameDesktop.svg';
 
 export const LogoDesktop = () => {
   return (
-    <Flex alignItems="center" gap={2} mb={2} px={2}>
+    <Link
+      as={ReactLink}
+      to="/search"
+      display="flex"
+      alignItems="center"
+      gap={2}
+      mb={2}
+      px={2}
+    >
       <Icon as={IconLogo} w={7} h={7} />
       <Icon as={NameLogo} w={20} />
-    </Flex>
+    </Link>
   );
 };
