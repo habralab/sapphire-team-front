395503024ee90017b46454e832bb42544bd1f6aa
Victoria Borovskaya <110840305+VictoriaBorovskaya@users.noreply.github.com>
2023-11-07 00:26:10 +0600

    disabled для настроек и уведомлений (#54)

--- src/features/user/settings/Settings.tsx
+++ src/features/user/settings/Settings.tsx
@@ -1,21 +1,24 @@
 import { Icon, IconButton } from '@chakra-ui/react';
 import { IoSettings } from 'react-icons/io5';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 import { useIsAuth } from '~/shared/hooks';
 import { PATHS } from '~/shared/lib/router';
 
 export function Settings() {
   const isAuth = useIsAuth();
+  const navigate = useNavigate();
+
   return (
     <IconButton
       isDisabled={!isAuth}
       size="sm"
       variant="flat"
       aria-label="settings"
-      to={PATHS.settings}
-      as={Link}
       icon={<Icon as={IoSettings} fontSize="2xl" />}
+      onClick={() => {
+        navigate(PATHS.settings);
+      }}
     />
   );
 }
