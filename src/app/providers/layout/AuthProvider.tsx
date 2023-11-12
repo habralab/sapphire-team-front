4d34c3e984639ad3f22e7f410c87ab41a630f6f0
Ruslan Kutliakhmetov <kosid2@gmail.com>
2023-11-12 10:25:49 +0500

    feat: глобальный loader

--- src/app/providers/layout/AuthProvider.tsx
+++ src/app/providers/layout/AuthProvider.tsx
@@ -1,9 +1,11 @@
+import { Box } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
 import { IsAuthResponse } from '~/shared/api';
 import { AuthContext, initAuth } from '~/shared/contexts';
 import { useApi } from '~/shared/hooks';
+import { Loader } from '~/shared/ui/Loader';
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
   const { userApi } = useApi();
@@ -36,6 +38,14 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
   }, [location.pathname, location.search]);
 
   return (
-    <AuthContext.Provider value={state}>{!loaded ? null : children}</AuthContext.Provider>
+    <AuthContext.Provider value={state}>
+      {!loaded ? (
+        <Box m="auto">
+          <Loader />
+        </Box>
+      ) : (
+        children
+      )}
+    </AuthContext.Provider>
   );
 }
