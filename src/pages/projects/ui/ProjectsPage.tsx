f53bb264d0ca609264c79c2d5c4e5d298661b766
Dmitriy Semenov <redead29@gmail.com>
2023-11-22 18:18:56 +0300

    Fix: изменение добавление обложки в проект (#67)

--- src/pages/projects/ui/ProjectsPage.tsx
+++ src/pages/projects/ui/ProjectsPage.tsx
@@ -1,8 +1,8 @@
 import { BasePageProps } from '~/shared/lib/router';
 
 import { NotAuthProjectsPage } from './NotAuthProjectPage';
-import { ProjectBase } from './ProjectBase';
+import { ProjectsBase } from './ProjectsBase';
 
 export const ProjectsPage = ({ user }: BasePageProps) => {
-  return !user.userId ? <NotAuthProjectsPage /> : <ProjectBase projectId={user.userId} />;
+  return !user.userId ? <NotAuthProjectsPage /> : <ProjectsBase userId={user.userId} />;
 };
