dc0e6b8a5f49403c2030db1e4a0d2e637b6f2e61
Ruslan Kutliakhmetov <kosid2@gmail.com>
2023-11-27 22:17:11 +0500

    fix: uppercase папки

---
+++ src/shared/ui/Status/Status.tsx
@@ -0,0 +1,24 @@
+import { Badge, BadgeProps, Text } from '@chakra-ui/react';
+
+type StatusProps = {
+  children?: string;
+} & BadgeProps;
+
+export const Status = (props: StatusProps) => {
+  const { children, ...others } = props;
+  return (
+    <Badge
+      bg={children === 'Проект завершён' ? 'gray.600' : 'purple.600'}
+      variant="solid"
+      textTransform="none"
+      px={2.5}
+      py={1}
+      borderRadius="full"
+      {...others}
+    >
+      <Text color="white" fontWeight="medium">
+        {children}
+      </Text>
+    </Badge>
+  );
+};
